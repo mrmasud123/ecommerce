85d738cca75fd5137599c41bcf4772d534dd7dbd
masud <mrmasud151821@gmail.com>
2026-08-05 17:52:59 +0600

    Changes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { createServer } = require('http')
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const next = require('next')
+import { createServer } from 'http'
+import next from 'next'
 
-const app = next({ dev: false })
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-    createServer((req, res) => handle(req, res)).listen(process.env.PORT || 3000, () => {
-        console.log('Next.js app ready')
-    })
+    createServer((req, res) => {
+        handle(req, res)
+    }).listen(port)
+
+    console.log(
+        `> Server listening at http://localhost:${port} as ${
+            dev ? 'development' : process.env.NODE_ENV
+        }`
+    )
 })
